bd72860544e74c559d00bf2a6484c9971fb13a04
koai-dev <khanhkimhn17@gmail.com>
2024-04-29 17:28:17 +0700

    1.4.10

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -109,7 +109,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.koai"
                 artifactId = "base"
-                version = "1.4.8"
+                version = "1.4.10"
 
                 afterEvaluate {
                     from(components["release"])
