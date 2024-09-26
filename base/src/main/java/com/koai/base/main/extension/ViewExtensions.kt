67057f39468b167d7ef2c60218393010143a18a8
koai-dev <koai.developer@gmail.com>
2024-09-26 11:02:36 +0700

    1.5.5 add safeClick

--- base/src/main/java/com/koai/base/main/extension/ViewExtensions.kt
+++ base/src/main/java/com/koai/base/main/extension/ViewExtensions.kt
@@ -1,5 +1,6 @@
 package com.koai.base.main.extension
 
+import android.os.SystemClock
 import android.view.View
 
 fun View.visible() {
@@ -12,4 +13,19 @@ fun View.gone() {
 
 fun View.invisible() {
     this.visibility = View.INVISIBLE
+}
+
+/**
+ * This function avoid double click -> Avoid user rapid click button.
+ */
+fun View.safeClick(debounceTime: Long = 800L, action: () -> Unit) {
+    this.setOnClickListener(object : View.OnClickListener {
+        private var lastClickTime: Long = 0
+
+        override fun onClick(v: View) {
+            if (SystemClock.elapsedRealtime() - lastClickTime < debounceTime) return
+            else action()
+            lastClickTime = SystemClock.elapsedRealtime()
+        }
+    })
 }
