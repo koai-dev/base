53041e2d34d0172b6ec687eb2262bcfe9f52a19b
koai-dev <koai.developer@gmail.com>
2024-09-30 17:37:07 +0700

    1.6.1 add endless recycleview

--- base/src/main/java/com/koai/base/network/BaseApiController.kt
+++ base/src/main/java/com/koai/base/network/BaseApiController.kt
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit
 
 abstract class BaseApiController<T : Any> {
     companion object {
-        const val TIME_OUT = 60L
+        const val TIME_OUT = 30L
     }
 
     fun getService(
